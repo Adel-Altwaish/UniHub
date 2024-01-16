5fdba8683f5ebc31effaef9da58063095cafbd55
Adel-Altweish <adel.fadi789@gmail.com>
2024-01-16 21:44:16 +0300

    add signIn With Google

--- lib/screen/sign_in_screen.dart
+++ lib/screen/sign_in_screen.dart
@@ -3,7 +3,7 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:uni_hub/widgets/custom_logo.dart';
 import 'package:uni_hub/widgets/my_button.dart';
 import 'package:uni_hub/widgets/text_form_field.dart';
@@ -20,6 +20,24 @@ class _SignInScreenState extends State<SignInScreen> {
   TextEditingController password = TextEditingController();
 
   GlobalKey<FormState> formstate = GlobalKey<FormState>();
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -160,22 +178,7 @@ class _SignInScreenState extends State<SignInScreen> {
                   ],
                 ),
                 onPressed: () {
-//                   Future<UserCredential> signInWithGoogle() async {
-//   // Trigger the authentication flow
-//   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-//   // Obtain the auth details from the request
-//   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-//   // Create a new credential
-//   final credential = GoogleAuthProvider.credential(
-//     accessToken: googleAuth?.accessToken,
-//     idToken: googleAuth?.idToken,
-//   );
-
-//   // Once signed in, return the UserCredential
-//   return await FirebaseAuth.instance.signInWithCredential(credential);
-// }
+                  signInWithGoogle();
                 },
               ),
               Container(height: 10),
